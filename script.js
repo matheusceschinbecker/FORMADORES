00685a91dd5b05eaf9c42d504887d0628934c340
matheusceschinbecker <matheus.ceschin.becker@escola.pr.gov.br>
2025-11-17 19:28:22 -0300

    aa

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 let texto = document.querySelector("#texto");
 texto.textContent = "Matheus";
+
 let nome = prompt("Qual é o seu nome?");
 texto.textContent = nome;
+
+if (nome == null){
+texto.textContent = 'Seja bem-vindo';
+}else{
+texto.textContent = nome;
+}
